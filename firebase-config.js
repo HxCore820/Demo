946981e2207019ea49311f3376836eb2269d5420
Nguyễn Hưng <258588158+HxCore820@users.noreply.github.com>
2026-02-05 22:48:26 +0700

    Change Firebase configuration settings
    Updated Firebase configuration with new project details.

--- firebase-config.js
+++ firebase-config.js
@@ -10,13 +10,13 @@
 window.FIREBASE_CONFIG = {
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   
-  apiKey: "AIzaSyCiNuPZNKL_Z1j5KHBvvi07iww1y02nO4Y",
-  authDomain: "githubrdp-6c83b.firebaseapp.com",
-  databaseURL: "https://githubrdp-6c83b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "githubrdp-6c83b",
-  storageBucket: "githubrdp-6c83b.firebasestorage.app",
-  messagingSenderId: "525203283735",
-  appId: "1:525203283735:web:08171ae95d85f875aca152",
-  measurementId: "G-T5B7VPCP04"
+  apiKey : "AIzaSyBpYfH_O3TGMNeROGFu_ghns0LPEId7hZU" , 
+  authDomain : "proxy-62231.firebaseapp.com" , 
+  databaseURL : "https://proxy-62231-default-rtdb.asia-southeast1.firebaseddatabase.app" , 
+  projectId : "proxy-62231" , 
+  storageBucket : "proxy-62231.firebasestorage.app" , 
+  messagingSenderId : "994524357611" , 
+  appId : "1:994524357611:web:ad7f21ac3181342a972fae" , 
+  measurementId : "G-QCLF8Z6R09"
  
 }
